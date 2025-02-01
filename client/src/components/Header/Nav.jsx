1757141202faab7bcc4f8d879d0735e7199b3514
Radoslav <todorovradoslav6@gmail.com>
2025-02-01 15:42:11 +0200

    Set up begining resourses and make header

---
+++ client/src/components/Header/Nav.jsx
@@ -0,0 +1,41 @@
+import {Link } from 'react-router-dom'
+
+export default function Nav(){
+    return(
+        <>
+               <nav className="navbar navbar-expand-lg fixed-top">
+        <div className="container">
+
+            <Link className="navbar-brand" to="/">FitZone</Link>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ml-lg-auto">
+                    <li className="nav-item">
+                        <Link to="/" className="nav-link smoothScroll">Home</Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link to="/fitzone/about" className="nav-link smoothScroll">About Us</Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link to="/fitzone/regitser" className="nav-link smoothScroll">Register</Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link to="/fitzone/login" className="nav-link smoothScroll">Login</Link>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+    </nav>
+        </>
+    )
+}
